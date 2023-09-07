5d8adba0230c6b194bb61bfb64498f7927c87e3a
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2023-09-08 01:35:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 window.addEventListener('scroll', e => {
 	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
 })
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
-ScrollSmoother.create({
-	wrapper: '.wrapper',
-	content: '.content'
-})
